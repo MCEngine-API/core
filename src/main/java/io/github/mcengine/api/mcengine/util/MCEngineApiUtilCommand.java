e92a2da6237bfefc849c8075d44d81c83b5a6ac3
JetsadaWijit <jetsadawijit@outlook.com>
2025-06-13 01:33:10 +0000

    fix: Add missing import

--- src/main/java/io/github/mcengine/api/mcengine/util/MCEngineApiUtilCommand.java
+++ src/main/java/io/github/mcengine/api/mcengine/util/MCEngineApiUtilCommand.java
@@ -2,6 +2,8 @@ package io.github.mcengine.api.mcengine.util;
 
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 /**
  * A utility class to dynamically register command executors by class name.
