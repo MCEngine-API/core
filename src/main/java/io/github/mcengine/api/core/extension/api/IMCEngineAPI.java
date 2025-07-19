4879ed290db69d36fa4d7253bdc383159b7e5074
JetsadaWijit <jetsadawijit@outlook.com>
2025-07-19 15:27:20 +0700

    fix(api): update Javadocs and references from DLC to API

--- src/main/java/io/github/mcengine/api/core/extension/api/IMCEngineAPI.java
+++ src/main/java/io/github/mcengine/api/core/extension/api/IMCEngineAPI.java
@@ -3,7 +3,7 @@ package io.github.mcengine.api.core.extension.api;
 import org.bukkit.plugin.Plugin;
 
 /**
- * Represents a core-related DLC module that can be dynamically loaded into the MCEngine.
+ * Represents a core-related API module that can be dynamically loaded into the MCEngine.
  * <p>
  * Implement this interface to create a plugin extension that hooks into the core system
  * provided by the MCEngine. The implementation should register its functionality within the {@link #onLoad(Plugin)} method.
@@ -11,17 +11,17 @@ import org.bukkit.plugin.Plugin;
 public interface IMCEngineAPI {
 
     /**
-     * Called when the DLC module is loaded by the engine.
+     * Called when the API module is loaded by the engine.
      * <p>
      * This method should be used to initialize any resources, register listeners,
      * or perform setup logic necessary for the plugin extension to function correctly.
      *
-     * @param plugin The {@link Plugin} instance that is providing the context for this DLC module.
+     * @param plugin The {@link Plugin} instance that is providing the context for this API module.
      */
     void onLoad(Plugin plugin);
 
     /**
-     * Called when the DLC module is unloaded or disabled by the engine.
+     * Called when the API module is unloaded or disabled by the engine.
      * <p>
      * Use this method to deregister any resources, cancel tasks,
      * or clean up systems related to this module.
